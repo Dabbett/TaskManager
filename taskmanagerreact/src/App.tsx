6e83c15db0af22faafbd8879f7366deab5c865db
Dylan Abbett <abbettdylan@gmail.com>
2024-04-05 10:08:24 -0400

    todo section layout and hooks

--- taskmanagerreact/src/App.tsx
+++ taskmanagerreact/src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './Components/InputField';
+import { Todo } from './Components/model';
+import TodoList from './Components/TodoList';
 
 
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault()
+
+    if(todo){
+      setTodos([...todos,{id: Date.now(), todo, isDone:false}]);
+      setTodo("")
+    }
+    
+  }
+
   return (
     <div className="App">
       <span className='heading'>TaskDrag</span>
-      <InputField/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos} />
+      
     </div>
   );
 }
--- taskmanagerreact/src/Components/InputField.tsx
+++ taskmanagerreact/src/Components/InputField.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './styles.css'
+import React, { useRef } from 'react'
+import './styles.css';
 
-const InputField = () => {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd:(e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}: Props) => {
+    const inputRef= useRef<HTMLInputElement>(null);
+
   return (
-    <form className='input'>
-      <input type='input' className= 'input-box' placeholder="Enter a task" ></input>
+    <form className='input' onSubmit={(e) => {
+        handleAdd(e)
+        inputRef.current?.blur();
+    }}>
+      <input
+        ref={inputRef}
+        type='input' 
+        value={todo}
+        onChange={
+            (e) => setTodo(e.target.value)
+            } 
+        className= 'input-box' 
+        placeholder="Enter a task"/>
       <button className='input-submit' type='submit'>Add</button>
     </form>
   )
